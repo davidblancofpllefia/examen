9e7d7a132a92c4ad7f8b1d8f9d46fed7072bd51c
David Macias <henriquemaciasdavid@fpllefia.com>
2025-05-28 18:34:52 +0200

    Desplegamiento

---
+++ src/docs/page.tsx
@@ -0,0 +1,4 @@
+// app/docs/page.tsx
+export default function DocsPage() {
+  return <h1 className="text-3xl font-bold">Docs</h1>;
+}
---
+++ src/pricing/page.tsx
@@ -0,0 +1,4 @@
+// app/pricing/page.tsx
+export default function PricingPage() {
+  return <h1 className="text-3xl font-bold">Pricing</h1>;
+}
